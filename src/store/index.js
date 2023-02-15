f9d73fed58171a0ea37ee97f5b3896d14aa9a64f
Samsyd <eabird99@gmail.com>
2023-02-15 22:35:37 +1100

    init 230215

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,23 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
-export default createStore({
-  state: {
+import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
+
+const store = createStore({
+  modules: {
+    coaches: coachesModule,
+    requests: requestsModule
+  },
+  state() {
+    return {
+      userId: 'c3'
+    };
   },
   getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    userId(state) {
+      return state.userId;
+    }
   }
-})
+});
+
+export default store;
